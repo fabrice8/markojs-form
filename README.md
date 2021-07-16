cd882f5cb1dbfafd9dec0b2d53354b10557ca9a2
Fabrice8 <aurphal2012@gmail.com>
2021-07-16 04:53:51 +0000

    Update README

--- README.md
+++ README.md
@@ -1,2 +1,304 @@
-# markojs-form
-Form Component Handler Library for MarkoJS
+@MarkoJS Form Handler
+==================================================
+
+Form Component Handler Library for [MarkoJS]. This library simplify how to handle the flow within a [MarkoJS] form component by handling all input fields **events**, **auto-fill**, **auto-completition**, **validation** and the **form submission** by binding to the component. Enough talking, let get to the facts.
+
+[![npm version][npm-badge]][npm]
+[![dependency status][dep-badge]][dep-status]
+
+[MarkoJS]: https://www.npmjs.org/package/markojs-form
+[npm]: https://www.npmjs.org/package/markojs-form
+[npm-badge]: https://img.shields.io/npm/v/markojs-form.svg?style=flat-square
+[dep-status]: https://david-dm.org/fabrice8/markojs-form
+[dep-badge]: https://img.shields.io/david/fabrice8/markojs-form.svg?style=flat-square
+
+
+## Installation
+
+Install using npm
+
+```shell
+$ npm install markojs-form
+```
+Install using yarn
+
+```shell
+$ yar add markojs-form
+```
+
+## Usage
+
+Create new instance of the FormHandler Object Class.
+
+```javascript
+const FormHandler = require('markojs-form'); // CommonJS
+
+// or 
+
+import FormHandler from 'markojs-form'; // ESM
+
+static const options = {
+    key: 'form-id',
+    autosave: true,
+    crosscheck: true
+}
+
+static const fhandler = new FormHandler( options );
+```
+
+**Note**: Every component should have its instance of form-handler
+
+## Options
+
+All options are optional
+
+* **key**: `String` Same as the component tag key of the form being handled: Eg. `<form key="form-id">...</form>`
+* **autosave**: `Boolean` Define whether to auto-save input values when the user is filling the form. Very useful especially in SPA case when the user manually or accidentally refresh the page, the inputs will stay filled with the saved values. Default `false`
+* **crosscheck**: `Boolean` When submit form event is detected by **FormHandler**, it run a final validation cross-check through all inputs that require validation in the form before submitting it. Default: `false`
+
+
+## Methods
+
+### `bind(this, [formObject])`
+
+Bind the form handler to the component. `formObject` is a object passed to the handler as the initial form fields & values: It's optional
+
+```javascript
+
+class {
+    onCreate(){
+        this.state = {
+            // ...
+        }
+
+        fhandler.bind(this)
+    }
+}
+```
+
+When this is done, the handler add `form` & `formError` fields to the component state: Eg. `this.state.form`. Those contain respectively the form data and the error status of the form validation.
+
+Also, couple of methods are added as well to the component automatically in order to listen to event that occured when filling & submitting the form.
+
+- ``__onInput`` Listen to input events 
+- ``__onChange`` Listen to input change events
+- ``__onSelect`` Listen to select change events
+- ``__onChecked`` Listen to checkbox and radio input events
+
+All these methods assigned approprietly to the various form tags, empowers the **FormHandler** to take care of the work of collecting and updating inputs state of the component.
+
+**Example**
+
+When filling the input form below, **FormHandler** automatically update `this.state.form.name` at every input & change events, allowing the component state to be updated.
+
+```HTML
+<input type="text"
+        name="name"
+        value=state.form.name
+        on-input("__onInput")
+        on-change("__onChange")>
+```
+
+**Note**: You can still assign your own method listener to handle inputs yourself. All is up to you.
+
+
+### `unbind(this)`
+
+Unbind the form handler to the component.
+
+
+### `set(name, value)`
+
+Manually set a value to a form input.
+
+```javascript
+class {
+    // ...
+    onMount(){
+        fhandler.set('fullname', 'John Dupont')
+    }
+}
+```
+
+### `fill(values)`
+
+Set method can only set on field at a time, while fill method get an object of fields & values to add manually to the form.
+
+```javascript
+class {
+    // ...
+    onInput(){
+        fhandler.fill({ status: 'healty', age: 79, gender: 'female' })
+    }
+}
+```
+
+### `error(name, [status])`
+
+Manually define the status of error of a given input. `status` can be a simple boolean: `true` meaning there's an error and `false` to cancel error. `status` can also be a string text message describing the error: Very usefull when the error must be displayed to the user.
+
+```javascript
+class {
+    // ...
+    onSubmit(){
+        fhandler.error('age', 'This field is required')
+    }
+    onDestry(){
+        // Cancel the error
+        fhandler.error('age', false)
+    }
+}
+
+<div>
+    <input type="number"
+            name="age"
+            value=state.form.age
+            on-change("__onChange")>
+    <!-- Dislay error message -->
+    <if( state.formError.age )>
+        <p>${state.formError.age}</p>
+    </if>
+</div>
+```
+
+### `reset()`
+
+Set the form back to its initial state. This method is automatilly call when the form get submited. It's also recommaned to call it when the form component `destroy` event occured.
+
+```javascript
+class {
+    onCreate(){
+        const initialForm = { username: 'gremdol', email: 'my@email.com' }
+
+        fhandler.bind(this, initialForm)
+    }
+    onDestroy(){
+        // Reset the form.
+        fhandler.reset()
+    }
+}
+```
+
+### `alert(message, status)`
+
+Display general alert of the form component
+
+```javascript
+class {
+    onSubmit(){
+        fhandler.alert('Unexpected Error Occured! Try again', 'warning')
+    }
+}
+```
+
+```HTML
+<FormAlert alert=state.alert/>
+```
+
+## Form Validation
+
+**FormHandler** has couple of in-build validation rules that you can use out of the box.
+- `required`: Specify that the input field is required
+- `url`: Check whether the input value is a valid URL
+- `domain`: Check whether the input value is a valid Domain/Hostname
+- `email`: Check whether the input value is a valid email address
+- `phone`: Check whether the input value is a valid phone number
+- `fullname`: Check whether the input value is a valid user fullname
+- `password`: check and return password by strengh
+- `number`: Input value must be a valid number
+- `boolean`: Input value must be True or False
+- `array`: Check whether the input value is an Array: Usually set manually
+- `object`: Check whether the input value is an Object: Usually set manually
+- `lowercase`: Input value must be only a lowercase string
+- `uppercase`: Input value must be onlye an uppercase string
+- `length`: Specify the required length of the input value
+- `minLength`: Specify the minimun length of the input value
+- `maxLength`: Specify the maximum length of the input value
+
+Anything that is not listed up there, can be define as custom rule with the function below
+
+### `define(rule)`
+
+Define custom validation rule. There are several ways of defining validation rules: 
+
+**Examples**
+
+```javascript
+class {
+    onCreate(){
+        fhandler.define([{ name: 'custom1', regexp: '/\.+/' }, { name: 'custom2', regexp: '/\w+/' }])
+    }
+}
+```
+```HTML
+<div>
+    <!-- Use inbuild rules -->
+    <input type="email"
+            name="email"
+            value=state.form.email
+            validate="email"
+            on-change("__onChange")>
+            
+    <!-- Use and already defined custom rules -->
+    <input type="text"
+            name="name"
+            value=state.form.name
+            validate="custom1"
+            on-change("__onChange")>
+            
+    <!-- Define custom rule and assign it right way -->
+    <input type="text"
+            name="orderId"
+            value=state.form.orderId
+            validate=fhandler.define([ name: 'orderId', regexp: '/PT-(\b+{4,})-00/' ])
+            on-change("__onChange")>
+</div>
+```
+
+### `validate(rule, value)`
+
+Manually validate a form field.
+
+
+## Events
+
+**FormHandler** instanciate the commong EventEmitter Object. Here a couple of events trigger by the form handler: 
+
+* `bind`: When **FormHandler** get bind to the component
+* `unbind`: When **FormHandler** get unbind to the component
+* `input`: New form input
+* `error`: When an error occured
+* `fill`: When `fill()` method get called
+* `autosave`: When new auto-save record occured
+* `reset`: When form get reset
+
+### `on(event, fn)`
+
+Declare an event listener
+
+### `off(event, fn)`
+
+Remove an event listener
+
+### `removeListeners()`
+
+Remove all event listeners
+
+
+Voilà!
+
+
+Feedback & Contribution
+-------
+
+You know the say: No one is whole alone! So, feedbacks and the smallest contributions you can think of are all welcome. Kindly report any encounted [Issues here][] and I'll be glad to work on it right away. Thank you.
+
+
+License
+-------
+
+This software is free to use under the MIT license. See the [LICENSE file][] for license text and copyright information.
+
+
+[LICENSE file]: https://github.com/fabrice8/markojs-form/blob/master/LICENSE
+[Issues here]: https://github.com/fabrice8/markojs-form/issues
---
+++ markojs-form.js
@@ -0,0 +1,469 @@
+/** Form Handler for @Markojs
+ * 
+ * @version 1.0.0
+ * @author Fabrice Marlboro
+ * @date 15/04/2021
+ * @repository https://github.com/fabrice8/markojs-form
+ * 
+ * @params options (Object)
+ * 		- key: <string> Scope identifier key of the form Element in the component
+ * 		- autosave: <true|false> Put the form on auto-save mode while the user is filling it.
+ * 		- crosscheck: <true|false> Re-test the validation patterns of each inputs in the form before submission
+ * 
+ * @Dependencies
+ * 		- UIStore Plugin for the auto-save feature. Check https://github.com/fabrice8/ui-store
+ */
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+function Patterns() {
+  const self = this,
+        PredefineList = {
+    required: value => {
+      return /[a-zA-Z0-9]/.test(value);
+    },
+    domain: value => {
+      return /^((http(s?)|ftp):\/\/)?[\w-]+(\.[\w-]+)/i.test(value);
+    },
+    url: value => {
+      return /^((http(s?)|ftp):\/\/)?[\w-]+(\.[\w-]+)+([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])?/i.test(value);
+    },
+    email: value => {
+      return /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,6}$/i.test(value);
+    },
+    phone: value => {
+      return self.number && /\d{7,}|(\d{2,} ) => {3,}/.test(value);
+    },
+    fullname: value => {
+      return /\s+/.test(value);
+    },
+    password: (value, type) => {
+      // validate a string words
+      let TYPES = {
+        weak: 1,
+        medium: 2,
+        strong: 3,
+        perfect: 4
+      },
+          stars = 0,
+          status; // Determine the level of password
+
+      if (/(?=.*[a-z].*[a-z].*[a-z])/.test(value)) stars++; // required string characters
+
+      if (/(?=.*[!@#$&*])/.test(value)) stars++; // required at least one special characters
+
+      if (/(?=.*[A-Z].*[A-Z])/.test(value)) stars++; // required at least one capital characters
+
+      if (/(?=.*[0-9].*[0-9])/.test(value)) stars++; // required numbers
+
+      if (/.{12,20}/.test(value)) stars += 2; // should length between 12 - 20 characters as long and strong password
+      else if (/.{8,12}/.test(value)) stars++; // should length between 8 - 12 characters as standard
+      // Redable password status
+
+      if (stars >= 0 && stars < 2) status = {
+        type: 'weak',
+        indice: 1
+      };else if (stars >= 2 && stars < 4) status = {
+        type: 'medium',
+        indice: 2
+      };else if (stars >= 4 && stars < 6) status = {
+        type: 'strong',
+        indice: 3
+      };else if (stars >= 6) status = {
+        type: 'perfect',
+        indice: 4
+      };
+      return TYPES.hasOwnProperty(type) && TYPES[type] <= status.indice;
+    },
+    number: value => {
+      return typeof value == 'number' || Number(value) !== null;
+    },
+    boolean: value => {
+      return typeof value == 'boolean';
+    },
+    array: value => {
+      return Array.isArray(value);
+    },
+    object: value => {
+      return typeof value == 'object' && !Array.isArray(value);
+    },
+    lowercase: value => {
+      return value.toLowerCase() == value;
+    },
+    uppercase: value => {
+      return value.toUpperCase() == value;
+    },
+    length: (value, size) => {
+      return value.length == parseInt(size);
+    },
+    minLength: (value, size) => {
+      return value.length >= parseInt(size);
+    },
+    maxLength: (value, size) => {
+      return value.length <= parseInt(size);
+    }
+  },
+        CustomList = {};
+
+  function extraSet(str) {
+    let combines = str.split(/\s*:\s*/);
+    return combines.length == 2 ? combines : [str, false];
+  }
+
+  function Testor(value, pattern, def) {
+    // Pattern validator not found
+    if (!PredefineList.hasOwnProperty(pattern) && !CustomList.hasOwnProperty(pattern)) return false;
+    return PredefineList.hasOwnProperty(pattern) && PredefineList[pattern](value, def) || // Pre-defiined pattern test
+    CustomList.hasOwnProperty(pattern) && new RegExp(CustomList[pattern]).test(value); // Custom patterns test
+  }
+
+  this.add = (name, regexp) => CustomList[name] = regexp;
+
+  this.has = pattern => {
+    // Check a defined pattern: Predefine or custom
+    return PredefineList.hasOwnProperty(pattern) || CustomList.hasOwnProperty(pattern);
+  };
+
+  this.test = (sequence, value) => {
+    // OR composed pattern
+    if (/\|/.test(sequence)) {
+      const results = sequence.split(/\s*\|\s*/).map(each => {
+        return self.test(each, value);
+      });
+      return results.includes(true);
+    } // Single or AND pattern
+
+
+    if (/\s+/.test(sequence)) return self.multiTest(sequence.split(/\s+/g), value); // pattern of multiple different type of value possible
+    else {
+        let [def] = extraSet(sequence);
+        return Testor(value, sequence, def);
+      }
+  };
+
+  this.multiTest = (sequenceList, value, extra) => {
+    // handle inputs that can have multiple different type of value possible
+    // Can have: Email or phone or date or ...
+    let state = false; // test if the value correspond to one of the type of the list
+
+    if (sequenceList.length) for (let o = 0; o < sequenceList.length; o++) {
+      let [pattern, def] = extraSet(sequenceList[o]);
+
+      if (!Testor(value, pattern, def)) {
+        state = false;
+        break;
+      } else state = true;
+    }
+    return state;
+  };
+}
+
+function getAttributes(inputs, type) {
+  const element = inputs.pop();
+  let value, name, pattern; // Standard HTML Element
+
+  if (typeof element.getAttribute == 'function') {
+    value = element.getAttribute('type') == 'number' ? parseInt(element.value) : element.value, name = element.getAttribute('name'), pattern = element.getAttribute('validate');
+  } // Markojs component
+  else {
+      value = inputs.pop() || element.input.value, name = element.input.name, pattern = element.input.validate;
+    }
+
+  switch (type) {
+    case 'checked':
+      value = element.checked;
+      break;
+  }
+
+  return {
+    value,
+    name,
+    pattern
+  };
+}
+
+function subSet(obj, link, data) {
+  const nsp = link.split('.'),
+        toCheck = nsp.shift();
+  if (!obj.hasOwnProperty(toCheck) && nsp.length) obj[toCheck] = {};
+
+  if (typeof obj == 'object' && nsp.length) {
+    obj = obj[toCheck];
+    return subSet(obj, nsp.join('.'), data);
+  }
+
+  obj[toCheck] = data;
+  return true;
+}
+
+let store = 'fh--auto-fill',
+    PEvents = ['bind', 'unbind', 'input', 'error', 'fill', 'autosave', 'reset'];
+/* Initialize the UI Store plugin */
+
+if (typeof window.uiStore === undefined) window.UIStore ? window.uiStore = new UIStore({
+  prefix: 'fh-00',
+  encrypt: true
+}) : console.log('[FormHandler] No UIStore Plugin No Found');
+
+function FormHandler(options) {
+  const self = this,
+        defaultOptions = {
+    autosave: false,
+    // Auto-save form while filling
+    crosscheck: true // Cross check validation pattern at submission
+
+  },
+        Events = {}; // Merge defined options with the default set.
+
+  this.options = Object.assign({}, defaultOptions, options || {}); // Auto-load saved data of this form
+
+  let savedData;
+
+  if (this.options.autosave) {
+    if (!this.options.key) throw new Error('[FormHandler] Undefined Form Key Identifier. Required for Auto-Save');
+    if (window.uiStore) savedData = (window.uiStore.get(store) || {})[this.options.key] || {};
+  }
+
+  function applyChange({
+    value,
+    name,
+    pattern
+  }) {
+    const toSet = {}; // Edit the form state
+
+    self.set(name, value);
+    self.formError[name] = false; // Test the validation pattern
+
+    if (pattern && !self.Patterns.test(pattern, value)) {
+      self.formError[name] = true;
+      fireEvent('error', [name, value]);
+    } // Apply changes
+
+
+    self.component.setStateDirty('formError', self.formError);
+  }
+
+  function definePattern({
+    name,
+    regexp
+  }) {
+    if (!name || !regexp) throw new Error('Invalid Pattern Defined. { <name>, <regexp> } expected'); // Remove regular expression closures "/"
+
+    regexp = String(regexp).replace(/^\/|\/$/g, '');
+
+    try {
+      new RegExp(regexp);
+    } catch (error) {
+      throw new Error('Invalid Regular Expression: ', error);
+    } // Add this to patterns bucket
+
+
+    self.Patterns.add(name, regexp);
+    return name;
+  }
+
+  function autoSave() {
+    if (!self.options.key || !self.options.autosave) return;
+    const allSaved = window.uiStore.get(store) || {};
+    allSaved[self.options.key] = self.form;
+    window.uiStore.set(store, allSaved);
+    fireEvent('autosave', [self.form]);
+  }
+
+  function autoClear() {
+    if (!self.options.key) return;
+    window.uiStore.update(store, self.options.key, 'delete');
+  }
+
+  function fireEvent(name, args) {
+    if (!Events.hasOwnProperty(name)) return;
+    Events[name].map(fn => Array.isArray(args) ? fn(...args) : fn());
+  }
+
+  this.form = {};
+  this.initForm = {};
+  this.formError = {};
+  this.formPatterns = {};
+  this.Patterns = new Patterns();
+  /*------------------------------------------------------------------------*/
+  // Bind this form to the container component
+
+  this.bind = (component, initForm) => {
+    // Binding component
+    this.component = component; // Initial form data or schema
+
+    this.initForm = initForm || {}; // populate auto-saved data with initial form data
+
+    this.form = Object.assign({}, this.initForm, savedData); // Set form initial states
+
+    component.setState({
+      form: this.form,
+      formError: this.formError
+    }); // Prevent this form from doing the default browser submission
+
+    if (this.options.key) {
+      let eForm;
+      component.on('mount', () => {
+        if (eForm = component.getEl(this.options.key)) eForm.onsubmit = e => e.preventDefault();
+      }).on('update', () => {
+        /* In case the component mount without the form,
+        check at any update whether the form is rendered
+        in case it's being manipulated with MarkoJS
+        conditional component <if><else>
+        */
+        if (eForm) return;
+        if (eForm = component.getEl(this.options.key)) eForm.onsubmit = e => e.preventDefault();
+      });
+    } // Assignable listeners to form's inputs
+
+
+    component.__onInput = (...args) => applyChange(getAttributes(args, 'input'));
+
+    component.__onChange = (...args) => applyChange(getAttributes(args, 'change'));
+
+    component.__onSelect = (...args) => applyChange(getAttributes(args, 'select'));
+
+    component.__onChecked = (...args) => applyChange(getAttributes(args, 'checked'));
+
+    fireEvent('bind');
+    return this;
+  }; // Unbind this form from the container component
+
+
+  this.unbind = component => {
+    component = component || this.component;
+    delete component.form;
+    delete component.formError;
+    delete component.__onInput;
+    delete component.__onChange;
+    delete component.__onSelect;
+    delete component.__onChecked;
+    fireEvent('unbind');
+    return this;
+  };
+  /*------------------------------------------------------------------------*/
+  // Set a field to the form
+
+
+  this.set = (name, data) => {
+    /\./.test(name) ? subSet(this.form, name, data) // Deep set
+    : this.form[name] = data; // shalow set
+
+    this.component.setStateDirty('form', this.form);
+    fireEvent('input', [name, data]); // Form auto-saving activated
+
+    if (this.options.autosave && window.uiStore) autoSave();
+    return this;
+  }; // Input data to fill the form
+
+
+  this.fill = data => {
+    if (typeof data !== 'object') return;
+    this.component.setStateDirty('form', Object.assign(this.form, data));
+    fireEvent('fill', [this.form]); // Form auto-saving activated
+
+    if (this.options.autosave && window.uiStore) autoSave();
+    return this;
+  }; // Set a field to the formError
+
+
+  this.error = (name, message) => {
+    this.formError[name] = message;
+    this.component.setStateDirty('formError', this.formError);
+    fireEvent('error', [name, message]);
+    return this;
+  }; // Manually reset the form
+
+
+  this.reset = () => {
+    this.form = {};
+    this.formError = {}; // Set form initial states
+
+    this.component.setState({
+      form: this.initForm,
+      formError: this.formError
+    }); // Clear existing form auto-save data
+
+    window.uiStore && autoClear();
+    fireEvent('reset');
+    return this;
+  }; // Push thrown form alert message to state to be display
+
+
+  this.alert = (message, type) => {
+    if (!message) return;
+    this.component.setStateDirty('alert', {
+      type: type || 'info',
+      message
+    });
+    const autoDismiss = setTimeout(() => {
+      this.component.setState('alert', null);
+      clearTimeout(autoDismiss);
+    }, 15000);
+    return this;
+  };
+  /*------------------------------------------------------------------------*/
+  // Define form & inputs validation patterns
+
+
+  this.define = arg => {
+    // Defined existing validation pattern
+    if (typeof arg == 'string') {
+      // Check declared pattern
+      arg.split(/\s+|\|/).map(each => {
+        const pattern = each.replace(/:(.+)$/, '');
+        if (!this.Patterns.has(pattern)) throw new Error('<' + each + '> is not a Predefined Validation Patterm');
+      });
+      return arg;
+    }
+    /*----------------------------------------------------------------------*/
+    // Custom validate patterns
+
+
+    let nameSet = []; // Array of custom validate patterns: Eg. [{ name: 'custom1', regexp: '/\.+/' }, { name: 'custom2', regexp: '/\w+/' }]
+
+    if (Array.isArray(arg)) arg.map(each => nameSet.push(definePattern(each)));
+    /*----------------------------------------------------------------------*/
+    // Object or single custom validate pattern: Eg. { name: 'custom', regexp: '/\.+/' }
+    else if (typeof arg == 'object') nameSet.push(definePattern(arg)); // Return the concatenated name of defined patterns
+
+    return nameSet.join(' ');
+  }; // Manual validation or pattern
+
+
+  this.validate = (pattern, value) => {
+    return this.Patterns.test(pattern, value);
+  };
+  /*------------------------------------------------------------------------*/
+  // Declare event listeners
+
+
+  this.on = (_event, fn) => {
+    // bounce unknow events
+    if (!PEvents.includes(_event)) return; // Register the listener
+
+    !Events.hasOwnProperty(_event) ? Events[_event] = [fn] // first listener
+    : Events[_event].push(fn); // additional listener
+
+    return this;
+  }; // Remove an event listeners
+
+
+  this.off = _event => {
+    delete Events[_event];
+    return this;
+  }; // Remove all event listeners
+
+
+  this.removeListeners = () => {
+    Events = {};
+    return this;
+  };
+}
+
+var _default = FormHandler;
+exports.default = _default;
